c870d39e35c892e026efc18b52ec38a11ec26065
SebastienFischer29 <sebastienfischer@hotmail.fr>
2025-03-04 15:11:31 +0100

    avancé page inscription inscription debut

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,66 +2,156 @@ $(init);
 
 function init() {
   /***********
-   * Boolen de validation de form
+   * Boolen de validation de form plus inuput si vraie
    */
-let bUserName = false;
-let bUserMail = false;
-let bUserMDP = false;
-/*********************
- * Récuperation valeur formulaire
- */
-let userName = "";
-let userMail = "";
-let userMDP = "";
+  let bUserName;
+  let bUserMail;
+  let bUserPsw;
+  let bSamePsw;
+  /*********************
+   * Récuperation valeur formulaire
+   */
+  let userName = "";
+  let userMail = "";
+  let userPsw = "";
+  let userPswC = "";
 
   /*****************
    * Page inscription
    ******************/
-  let name = $('#name');// gestion du nom 
-  bUserName = name.on("input",nameLenght);/********* */
 
-  let mail = $('#email');
-  bUserMail = mail.on("input",mailRegex);
-
+  $('#name').on("input", nameLenght);
+  $('#email').on("input", mailRegex);
+  $('#psw').on("input", pswPattern);
+  $('#pswConfirm').on("input", pswConfirm);
+  $('#reset').on("click", formAnnul);
+  $('createAccount').on("click", inscription);
 }
 /****************
  * Function
  ***************/
 function nameLenght() {
   let textName = $('#errorName');//*****/
-  let img = $( "#name:first-child");
-  console.log(img);
-  
-  let isOk = /.{3,}/.test(this.value);  
+  let img = $("#imgName");
+  let isOk = /.{3,}/.test($(this).val());
+  console.log($('#name').val());
   if (isOk) {
-      textName.css("color", "black");
-      return true ;
+    textName.css("color", "black");
+    img.attr('src', "../images/check.svg");
+    bUserName = { b: true, name: ($(this).val()) };
   } else {
     textName.text("Choisissez un pseudo contenant au moins 3 caractères");
     textName.css("color", "red");
-    return false;
+    img.attr('src', "../images/error.svg");
+    if ($('#name').val() == "") {
+      $("#imgName").attr('src', "");
+    }
+    bUserName = false;
   }
 }
-function mailRegex(){
-  let textMail = $('errorMail');
+function mailRegex() {
+  let textMail = $('#errorMail');
+  let img = $("#imgMail");
   let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  let isOk = regex.test(this.value);
-  console.log(isOk);
+  let isOk = regex.test($(this).val());
   if (isOk) {
     textMail.css("color", "black");
-    return true ;
-} else {
-  textMail.text("Rentrez un mail valide");
-  textMail.css("color", "red");
-  return false;
-}
+    img.attr('src', "../images/check.svg");
+    bUserMail = { b: true, mail: ($(this).val()) };
+  } else {
+    textMail.text("Rentrez un mail valide");
+    img.attr('src', "../images/error.svg");
+    textMail.css("color", "red");
+    if ($('#email').val() == "") {
+      $("#imgMail").attr('src', "");
+    }
+    bUserMail = false;
+  }
 
 }
+function pswPattern() {
+
+  let textPsw = $('#errorPsw');
+  let img = $("#imgPsw");
+
+
+  let regexPsw = /^(?=.*\d)(?=.*[\W_]).{6,}$/;
+  let isOk = regexPsw.test(this.value);
+
+
+  if ($(this).val().length == 0) {
+    $("#faible").css('visibility', 'hidden');
+    $("#moyen").css('visibility', 'hidden');
+    $("#fort").css('visibility', 'hidden');
+  }
+  if ($(this).val().length <= 4) {
+    $("#faible").css('visibility', 'visible');
+    $("#moyen").css('visibility', 'hidden');
+    $("#fort").css('visibility', 'hidden');
+  }
+  if ($(this).val().length > 4 && $(this).val().length <= 8) {
+    $("#moyen").css('visibility', 'visible');
+    $("#fort").css('visibility', 'hidden');
+  }
+  if ($(this).val().length > 8) {
+    $("#fort").css('visibility', 'visible');
+  }
+
+  if (isOk) {
+    textPsw.css("color", "black");
+    img.attr('src', "../images/check.svg");
+    bUserPsw = { b: true, password: $(this).val() };
+  } else {
+    textPsw.text("Au moins un symbole, un chiffre, ainsi que 6 caractères minimum.");
+    img.attr('src', "../images/error.svg");
+    textPsw.css("color", "red");
+    if ($('#psw').val() == "") {
+      $("#imgPsw").attr('src', "");
+    }
+    bUserPsw = false;
+  }
+}
+function pswConfirm() {
+  let textPsw = $('#errorPswC');
+  let img = $("#imgPswC");
+
+  let isOk = $('#psw').val() == $('#pswConfirm').val();
+
+  if (isOk) {
+    textPsw.css("color", "black");
+    img.attr('src', "../images/check.svg");
+    bSamePsw = true;
+  } else {
+    textPsw.text("Les deux mots de passe ne sont pas identiques.");
+    img.attr('src', "../images/error.svg");
+    textPsw.css("color", "red");
+    if ($('#pswConfirm').val() == "") {
+      $("#imgPswC").attr('src', "");
+    }
+    bSamePsw = false;
+  }
+}
+function formAnnul() {
+  $("#imgName").attr('src', "");
+  $("#imgMail").attr('src', "");
+  $("#imgPsw").attr('src', "");
+  $("#imgPswC").attr('src', "");
+  $("#faible").css('visibility', 'hidden');
+  $("#moyen").css('visibility', 'hidden');
+  $("#fort").css('visibility', 'hidden');
+}
+function inscription() {
+
+
+
+
+}
+
 /****************
  * Fin Function
  ***************/
 
 
-  /*****************
-   * Fin Page inscription
-   ******************/
+/*****************
+ * Fin Page inscription
+ ******************/
