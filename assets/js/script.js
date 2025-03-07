8fb4b332a9e7c8d7259e1ae1c699927a77c305b1
SebastienFischer29 <sebastienfischer@hotmail.fr>
2025-03-07 13:34:37 +0100

    Fin projet2

--- assets/js/script.js
+++ assets/js/script.js
@@ -389,6 +389,11 @@ function themesCard() {
   let users = getUsers();
   let userConnect = users.find(user => user.connect === true);
 
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+
   switch (userConnect.memory) {
     case "alphabet-scrabble": return ["1.png", "2.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png", "16.png", "17.png", "18.png", "19.png", "20.png", "21.png", "22.png", "23.png", "24.png", "25.png", "26.png"];
       break;
@@ -541,4 +546,4 @@ function recordingChoice() {
  ******************/
 
 /**************** */
-/*********************************************************************************************
+/*********************************************************************************************/
